89b70a0f1599f9c563f23070fff90e94bdff6404
Christian Roese <croese@tada.today>
2018-08-27 11:14:34 -0500

    Adds Canvas and tests

---
+++ NRayTracer.Core/Canvas.cs
@@ -0,0 +1,28 @@
+﻿namespace NRayTracer.Core
+{
+    public class Canvas
+    {
+        private readonly Color[] _pixels;
+
+        public Canvas(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            _pixels = new Color[width * height];
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public Color GetPixel(int x, int y)
+        {
+            return _pixels[y * Width + x];
+
+        }
+
+        public void WritePixel(int x, int y, Color color)
+        {
+            _pixels[y * Width + x] = color;
+        }
+    }
+}
---
+++ NRayTracer.Tests/CanvasTest.cs
@@ -0,0 +1,32 @@
+﻿using NRayTracer.Core;
+using Xunit;
+
+namespace NRayTracer.Tests
+{
+    public class CanvasTest
+    {
+        [Fact]
+        public void CanCreateACanvas()
+        {
+            var c = new Canvas(10, 20);
+            Assert.Equal(10, c.Width);
+            Assert.Equal(20, c.Height);
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    Assert.Equal(new Color(0, 0, 0), c.GetPixel(i, j));
+                }
+            }
+        }
+
+        [Fact]
+        public void CanSetACanvasPixel()
+        {
+            var c = new Canvas(10, 20);
+            var red = new Color(1, 0, 0);
+            c.WritePixel(2, 3, red);
+            Assert.Equal(red, c.GetPixel(2, 3));
+        }
+    }
+}
