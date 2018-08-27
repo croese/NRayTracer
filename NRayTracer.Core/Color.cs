26e56d5badf370fa4ddb2c0b2fce39a59217f2cd
Christian Roese <croese@tada.today>
2018-08-27 10:21:22 -0500

    Implements Color struct and tests

--- NRayTracer.Core/Color.cs
+++ NRayTracer.Core/Color.cs
@@ -1,8 +1,55 @@
 ﻿using System;
+
 namespace NRayTracer.Core
 {
-  public struct Color
-  {
+    public struct Color : IEquatable<Color>
+    {
+        public Color(double red, double green, double blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
 
-  }
+        public double Red { get; }
+        public double Green { get; }
+        public double Blue { get; }
+
+        public bool Equals(Color other)
+        {
+            return FloatMath.AreEqual(Red, other.Red)
+                            && FloatMath.AreEqual(Green, other.Green)
+                            && FloatMath.AreEqual(Blue, other.Blue);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Color))
+            {
+                return false;
+            }
+
+            return Equals((Color)obj);
+        }
+
+        public static Color operator +(Color lhs, Color rhs)
+        {
+            return new Color(lhs.Red + rhs.Red, lhs.Green + rhs.Green, lhs.Blue + rhs.Blue);
+        }
+
+        public static Color operator -(Color lhs, Color rhs)
+        {
+            return new Color(lhs.Red - rhs.Red, lhs.Green - rhs.Green, lhs.Blue - rhs.Blue);
+        }
+
+        public static Color operator *(Color c, double factor)
+        {
+            return new Color(c.Red * factor, c.Green * factor, c.Blue * factor);
+        }
+
+        public static Color operator *(Color lhs, Color rhs)
+        {
+            return new Color(lhs.Red * rhs.Red, lhs.Green * rhs.Green, lhs.Blue * rhs.Blue);
+        }
+    }
 }
---
+++ NRayTracer.Tests/ColorTest.cs
@@ -0,0 +1,48 @@
+﻿using NRayTracer.Core;
+using Xunit;
+
+namespace NRayTracer.Tests
+{
+    public class ColorTest
+    {
+        [Fact]
+        public void CanCreateAColor()
+        {
+            var c = new Color(-0.5, 0.4, 1.7);
+            Assert.True(FloatMath.AreEqual(c.Red, -0.5));
+            Assert.True(FloatMath.AreEqual(c.Green, 0.4));
+            Assert.True(FloatMath.AreEqual(c.Blue, 1.7));
+        }
+
+        [Fact]
+        public void ColorsCanBeAdded()
+        {
+            var c1 = new Color(0.9, 0.6, 0.75);
+            var c2 = new Color(0.7, 0.1, 0.25);
+            Assert.Equal(new Color(1.6, 0.7, 1.0), c1 + c2);
+        }
+
+        [Fact]
+        public void ColorsCanBeSubtracted()
+        {
+            var c1 = new Color(0.9, 0.6, 0.75);
+            var c2 = new Color(0.7, 0.1, 0.25);
+            Assert.Equal(new Color(0.2, 0.5, 0.5), c1 - c2);
+        }
+
+        [Fact]
+        public void ColorsCanBeMultipliedByAScalar()
+        {
+            var c = new Color(0.2, 0.3, 0.4);
+            Assert.Equal(new Color(0.4, 0.6, 0.8), c * 2);
+        }
+
+        [Fact]
+        public void ColorsCanBeMultiplied()
+        {
+            var c1 = new Color(1, 0.2, 0.4);
+            var c2 = new Color(0.9, 1, 0.1);
+            Assert.Equal(new Color(0.9, 0.2, 0.04), c1 * c2);
+        }
+    }
+}
